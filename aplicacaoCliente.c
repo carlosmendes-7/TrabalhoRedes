dc26277269833ec438b67abc3d13d6570350f866
barbudan <barbudan@DESKTOP-CAAGM7D.localdomain>
2019-11-04 22:57:33 -0300

    Indentacao no codigo-fonte

--- aplicacaoCliente.c
+++ aplicacaoCliente.c
@@ -20,11 +20,11 @@ void func(int sockfd); //TESTANDO MSG DE TEXTO COM SERVIDOR
 
 int main(int argc, char *argv[])
 {
-
 	char serverAddress[INET6_ADDRSTRLEN] = ""; // INET6_ADDRSTRLEN = 46 bytes
 	int rc = 0; /*variavel que recebe o retorno das chamadas de funcao*/
 
-	if (argc < 2) {
+	if (argc < 2)
+    {
         showHelp(argv[0]); // Mostra helper caso usuario nao defina nenhuma acao apos execucao do programa(-h ou -s)
     }
 
@@ -36,33 +36,32 @@ int main(int argc, char *argv[])
         {"send"   , required_argument , NULL , 's'},
         {"enviar"  , required_argument , NULL , 's'},
         {0          , 0                 , 0    ,  0 },
-	} ;
+	};
 
 	// Verifica a corretude dos comandos digitados
 	while ( (opt = getopt_long(argc, argv, "hs:", longopts, NULL)) > 0 )
 	{
-		switch ( opt ) {
-        case 'h': /* -h ou --help */
-            showHelp(argv[0]);
-            break ;
-        case 's': /* -s ou --send */
-            if(strlen(optarg) >= INET6_ADDRSTRLEN){
-            	fprintf(stderr, "Erro: endereco de destino muito grande\n") ;
-            	exit(EXIT_FAILURE);
-            }
-
-            strcpy(serverAddress, optarg); // coloca endereco escolhido pelo usuario na string 'server'
-
-            if(argv[optind] == NULL)
-            {
-                printf("Erro: argumento [ARQUIVO] nao encontrado\nEncerrando aplicacao...\n");
-                exit(EXIT_FAILURE);
-            }
-
-            break ;
-        default:
-            fprintf(stderr, "Erro: opcao invalida ou faltando argumento: '%c'\n", optopt) ;
-            exit(EXIT_FAILURE); 
+		switch ( opt )
+        {
+            case 'h': /* -h ou --help */
+                showHelp(argv[0]);
+                break;
+            case 's': /* -s ou --send */
+                if(strlen(optarg) >= INET6_ADDRSTRLEN)
+                {
+                	fprintf(stderr, "Erro: endereco de destino muito grande\n");
+                	exit(EXIT_FAILURE);
+                }
+                strcpy(serverAddress, optarg); // coloca endereco escolhido pelo usuario na string 'server'
+                if(argv[optind] == NULL)
+                {
+                    printf("Erro: argumento [ARQUIVO] nao encontrado\nEncerrando aplicacao...\n");
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            default:
+                fprintf(stderr, "Erro: opcao invalida ou faltando argumento: '%c'\n", optopt);
+                exit(EXIT_FAILURE); 
 		}
 	}
 
@@ -78,37 +77,43 @@ int main(int argc, char *argv[])
     servaddr = defineEndereco("127.0.0.1");
   
     // connect the client socket to server socket 
-    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0)
+    { 
         printf("connection with the server failed...\n"); 
         exit(0); 
     } 
     else
+    {
         printf("connected to the server..\n"); 
+    }
     
     // function for chat 
     func(sockfd); 
   
     // close the socket 
     close(sockfd); 
-    
+
     /////////////////////////////////////
     
 	while ( argv[optind] != NULL )
 	{
 		struct stat s ;
-        if ( stat(argv[optind], &s) == -1 ){
+        if ( stat(argv[optind], &s) == -1 )
+        {
         	printf("Erro em arquivo \"%s\": %s\n", argv[optind], strerror(errno));
         	++optind;
         	continue;
         }
-        if( enviarArquivo(argv[optind], serverAddress, s.st_blocks) ) {
+        if( enviarArquivo(argv[optind], serverAddress, s.st_blocks) )
+        {
         	printf("Arquivo \"%s\" transmitido com sucesso!\n", argv[optind]);
-        }else{
+        }
+        else
+        {
         	fprintf(stderr, "Erro: falha na transmissao do arquivo \"%s\"\n", argv[optind]);
         }
         ++optind;
     }
-
 	return 0;
 }
 
@@ -125,17 +130,21 @@ void func(int sockfd)
 { 
     char buff[MAX]; 
     int n; 
-    for (;;) { 
+    for (;;)
+    { 
         bzero(buff, sizeof(buff)); 
         printf("Enter the string : "); 
         n = 0; 
-        while ((buff[n++] = getchar()) != '\n') 
-            ; 
+        
+        while ((buff[n++] = getchar()) != '\n');
+
         write(sockfd, buff, sizeof(buff)); 
         bzero(buff, sizeof(buff)); 
         read(sockfd, buff, sizeof(buff)); 
         printf("From Server : %s", buff); 
-        if ((strncmp(buff, "exit", 4)) == 0) { 
+        
+        if ((strncmp(buff, "exit", 4)) == 0)
+        { 
             printf("Client Exit...\n"); 
             break; 
         } 
--- aplicacaoServidor.c
+++ aplicacaoServidor.c
@@ -28,30 +28,40 @@ int main(int argc, char *argv[])
     servaddr = defineEndereco(INADDR_ANY);
 
     // Binding newly created socket to given IP and verification 
-    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
+    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0)
+    { 
         printf("socket bind failed...\n"); 
         exit(0); 
     } 
     else
+    {
         printf("Socket successfully binded..\n"); 
+    }
   
     // Now server is ready to listen and verification
-    if ((listen(sockfd, 5)) != 0) { 
+    if ((listen(sockfd, 5)) != 0)
+    { 
         printf("Listen failed...\n"); 
         exit(0); 
     } 
     else
+    {
         printf("Server listening..\n"); 
+    }
+
     len = sizeof(cli); 
   
     // Accept the data packet from client and verification 
     connfd = accept(sockfd, (SA*)&cli, &len); 
-    if (connfd < 0) { 
+    if (connfd < 0)
+    { 
         printf("server acccept failed...\n"); 
         exit(0); 
     } 
     else
+    {
         printf("server acccept the client...\n");
+    }
 
     // Function for chatting between client and server 
     func(connfd); 
@@ -66,7 +76,8 @@ void func(int sockfd)
     char buff[MAX]; 
     int n; 
     // infinite loop for chat 
-    for (;;) { 
+    for (;;)
+    { 
         bzero(buff, MAX); 
   
         // read the message from client and copy it in buffer 
@@ -76,14 +87,14 @@ void func(int sockfd)
         bzero(buff, MAX); 
         n = 0; 
         // copy server message in the buffer 
-        while ((buff[n++] = getchar()) != '\n') 
-            ; 
+        while ((buff[n++] = getchar()) != '\n'); 
   
         // and send that buffer to client 
         write(sockfd, buff, sizeof(buff)); 
   
         // if msg contains "Exit" then server exit and chat ended. 
-        if (strncmp("exit", buff, 4) == 0) { 
+        if (strncmp("exit", buff, 4) == 0)
+        { 
             printf("Server Exit...\n"); 
             break; 
         } 
